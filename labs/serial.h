14074526a3f24e25d9be78112614f98b1ff83fb1
Sarthak Singla <sarthak44singla@gmail.com>
2022-01-19 17:03:32 +0530

    Parts 1.1, 1.2, 1.3

--- labs/serial.h
+++ labs/serial.h
@@ -6,10 +6,12 @@ namespace serial{
 
   static inline bool is_transmitter_ready(io_t baseport){
     //insert your code here
+    return(1==io::read8(baseport,2));
   }
 
   static inline void writechar(uint8_t c, io_t baseport){
     //insert your code here
+    io::write8(baseport, (uint16_t)0, c);
   }
 
 } //end serial
--- labs/vgatext.h
+++ labs/vgatext.h
@@ -1,9 +1,11 @@
 #include "util/io.h"
 
+
 namespace vgatext{
 
    static inline void writechar(int loc, uint8_t c, uint8_t bg, uint8_t fg, addr_t base){
      //your code goes here
+    mmio::write16(base, loc<<1, ((uint16_t)(((uint16_t)bg<<(uint16_t)12)+((uint16_t)fg<<(uint16_t)8)+(uint16_t)c)));
    }
 
 }//namespace vgatext
