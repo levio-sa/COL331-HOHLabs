4db732226d65c7be7a3b8069ca2819f79ff1ed9f
RaviSriTejaKuriseti <padukuriseti@gmail.com>
2022-01-23 18:44:33 +0530

    Update fibonacci

--- labs/shell.cc
+++ labs/shell.cc
@@ -257,9 +257,26 @@ int factorial(int arg){
 }
 
 
-int fib(int arg){
-  // if()
-  return arg;
+int fib(int n){
+
+  int arr[50];
+  if(n==0){
+    return 0;
+  }
+  else if(n==1){
+    return 1;
+  }
+  else{
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++){
+      arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+    
+    }
+
+  //return arg;
 }
 
 //
